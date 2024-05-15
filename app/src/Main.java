c020cc83152f1312ab2b5465b19fe19e44632265
GabrielbrandaoRP <gabrielhand15@gmail.com>
2024-05-15 15:15:41 -0300

    Create

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        Pessoa p = new Pessoa();
+        int servico = 2;
+
+        switch (servico){
+            case 1:
+                System.out.println("Cadastrar ");
+                break;
+            case 2:
+                System.out.println("Efetuar login");
+                break;
+            case 3:
+                System.out.println("Escolher serviço");
+                break;
+
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
     }
 }
--- app/src/Pessoa.java
+++ app/src/Pessoa.java
@@ -3,4 +3,20 @@ public class Pessoa {
     protected String nome;
     protected String CPF;
 
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String next) {
+        this.CPF = CPF;
+    }
 }
